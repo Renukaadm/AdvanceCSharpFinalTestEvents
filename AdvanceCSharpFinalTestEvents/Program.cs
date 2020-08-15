023031e5abec9a7830a2ef57fa94285392910067
Renuka <renuka.aedma@gmail.com>
2020-08-15 12:23:03 -0400

    FirstCommit

--- AdvanceCSharpFinalTestEvents/Program.cs
+++ AdvanceCSharpFinalTestEvents/Program.cs
@@ -6,7 +6,57 @@ namespace AdvanceCSharpFinalTestEvents
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //5)    Associate the event with the event handler
+            var eventclass = new EventClass();
+            eventclass.name = "Renuka";
+
+            var eventHandleClass = new EventHandleClass();
+            eventHandleClass.MyEvent += eventclass.HandleEvent;
+
+            //calling the event to occur
+            eventHandleClass.EventHandlerMethod();
         }
     }
+    //4)    Create code the will be run when the event occurs
+    public class EventClass
+    {
+        public string name { get; set; }
+
+        public void HandleEvent(object sender, MyEventArgs e)
+        {
+            Console.WriteLine("HadlingEvent {0}", e.message);
+        }
+    }
+
+    //3)    Declare the code for the event
+    public class EventHandleClass
+    {
+        public event MyEventHandeler MyEvent;
+
+        public void EventHandlerMethod()
+        {
+            
+            MyEventHandeler myHandler = MyEvent;
+            if (myHandler != null)
+            {
+                myHandler(this, new MyEventArgs("My Event Handler"));
+            }
+
+        }
+    }
+
+    //2)    Set up the delegate for the event
+    public delegate void MyEventHandeler(object source, MyEventArgs e);
+
+    //1)    Create a class to pass as an argument for the event handlers
+    public class MyEventArgs : EventArgs
+    {
+        public string message;
+        public MyEventArgs(string str)
+        {
+            message = str;
+        }
+
+    }
+
 }
